72af9e9a4c0b54da91707c1e866d4f086574d2b4
nilpe <knohitokasou@gmail.com>
2024-07-19 18:02:30 +0900

    chore: Remove unused test_driver.cpp file and update test_driver.h with new ioctl commands
    Change-Id: I8c99d1d286f9c333e7262bf62eef88ece8713697

--- src/add-ons/kernel/drivers/test2/test_driver.c
+++ src/add-ons/kernel/drivers/test2/test_driver.c
@@ -1,139 +1,223 @@
 /*
+../misc/test.cより雛形を拝借
+以下はそれについていたcopyright
+*/
+
+/*
  * Copyright (c) 2007 Marcus Overhagen <marcus@overhagen.de>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction, 
- * including without limitation the rights to use, copy, modify, 
- * merge, publish, distribute, sublicense, and/or sell copies of 
- * the Software, and to permit persons to whom the Software is 
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be 
+ * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
-
-#include <KernelExport.h>
+#define FIFO_SIZE 1024
+#include "test_driver.h"
 #include <Drivers.h>
 #include <Errors.h>
+#include <KernelExport.h>
+#include <stdlib.h>
+#include <string.h>
 
-int32 			api_version = B_CUR_DRIVER_API_VERSION;
-static int32	sOpenMask;
+static sem_id devMutex;
+static sem_id rwMutex;
+static char FIFO[FIFO_SIZE + 1];
+static int charNum = 0;
+static enum testdevice_state state = TESTDEVICE_STATE_ENABLE;
 
-status_t
-init_hardware(void)
-{
-	dprintf("test: init_hardware\n");
-	return B_OK;
+int32 api_version = B_CUR_DRIVER_API_VERSION;
+static int32 sOpenMask;
+
+int copy_from_user(void *kernel, const void *user, int len) {
+  if (user == NULL) {
+    return B_BAD_VALUE;
+  }
+
+  if (!IS_USER_ADDRESS(user)) {
+    return B_BAD_ADDRESS;
+  }
+
+  return user_memcpy(kernel, user, len);
 }
 
+int copy_to_user(void *user, const void *kernel, int len) {
+  if (user == NULL) {
+    return B_BAD_VALUE;
+  }
 
-status_t
-init_driver(void)
-{
-	dprintf("test: init_driver\n");
-	return B_OK;
+  if (!IS_USER_ADDRESS(user)) {
+    return B_BAD_ADDRESS;
+  }
+
+  return user_memcpy(user, kernel, len);
 }
 
-
-void
-uninit_driver(void)
-{
-	dprintf("test: uninit_driver\n");
+status_t init_hardware(void) {
+  dprintf("test: init_hardware\n");
+  return B_OK;
 }
 
-
-static status_t
-driver_open(const char *name, uint32 flags, void** _cookie)
-{
-	dprintf("test: open\n");
-
-	if (atomic_or(&sOpenMask, 1)) {
-		dprintf("test: open, BUSY!\n");
-		return B_BUSY;
-	}
-
-	dprintf("test: open, success\n");
-	return B_OK;
+status_t init_driver(void) {
+  dprintf("test: init_driver\n");
+  devMutex = create_sem(1, "devMutex");
+  if (devMutex < B_OK) {
+    return B_NO_MEMORY;
+  }
+  rwMutex = create_sem(1, "rwMutex");
+  if (rwMutex < B_OK) {
+    return B_NO_MEMORY;
+  }
+  memset(FIFO, 0, sizeof(FIFO));
+  return B_OK;
 }
 
-
-static status_t
-driver_close(void* cookie)
-{
-	dprintf("test: close enter\n");
-	snooze(200000);
-	dprintf("test: close leave\n");
-	return B_OK;
+void uninit_driver(void) {
+  dprintf("test: uninit_driver\n");
+  delete_sem(devMutex);
+  delete_sem(rwMutex);
 }
 
+static status_t driver_open(const char *name, uint32 flags, void **_cookie) {
+  dprintf("test: open\n");
 
-static status_t
-driver_free(void* cookie)
-{
-	dprintf("test: free\n");
-	atomic_and(&sOpenMask, ~1);
-	return B_OK;
+  status_t ok = acquire_sem(devMutex);
+  if (ok != B_OK) {
+    return ok;
+  }
+  dprintf("test: open, success\n");
+  return B_OK;
 }
 
-
-static status_t
-driver_read(void* cookie, off_t position, void *buf, size_t* num_bytes)
-{
-	dprintf("test: read\n");
-	*num_bytes = 0; // nothing to read
-	return B_ERROR;
+static status_t driver_close(void *cookie) {
+  dprintf("test: close enter\n");
+  release_sem(devMutex);
+  dprintf("test: close leave\n");
+  return B_OK;
 }
 
+static status_t driver_free(void *cookie) {
+  dprintf("test: free\n");
 
-static status_t
-driver_write(void* cookie, off_t position, const void* buffer, size_t* num_bytes)
-{
-	dprintf("test: write\n");
-	snooze(1000000);	// 1s
-	*num_bytes = 1;		// pretend 1 byte was written
-	return B_OK;
+  return B_OK;
 }
 
+static status_t driver_read(void *cookie, off_t position, void *buf,
+                            size_t *num_bytes) {
+  dprintf("test: read\n");
 
-static status_t
-driver_control(void *cookie, uint32 op, void *arg, size_t len)
-{
-	dprintf("test: control\n");
-	return B_ERROR;
+  dprintf("Reading from device\n");
+  dprintf("charNum = %d\n", charNum);
+  if (state == TESTDEVICE_STATE_DISABLE) {
+    dprintf("Device is disabled\n");
+    return B_IO_ERROR;
+  }
+
+  if (*num_bytes == 0 || charNum == 0) {
+    return 0;
+  }
+  int Num2Read = min(*num_bytes, (size_t)charNum);
+
+  if (Num2Read == 0) {
+    return B_OK;
+  } else if (Num2Read > FIFO_SIZE) {
+    Num2Read = FIFO_SIZE;
+  }
+  char *kbuf = malloc(Num2Read);
+  if (kbuf == NULL) {
+    dprintf("Error in malloc\n");
+    return -ENOMEM;
+  }
+  for (int i = 0; i < Num2Read; i++) {
+    kbuf[i] = FIFO[i];
+  }
+  // kbuf[Num2Read] = '\0';
+  int t = copy_to_user(buf, kbuf, Num2Read);
+  if (t != 0) {
+    dprintf("Error in copy_to_user\n");
+    Num2Read = Num2Read - t;
+  }
+  for (int i = 0; i < charNum - Num2Read; i++) {
+    if (i + Num2Read >= FIFO_SIZE) {
+      break;
+    }
+    FIFO[i] = FIFO[i + Num2Read];
+  }
+  charNum = charNum - Num2Read;
+  *num_bytes = Num2Read;
+  return B_OK;
 }
 
+static status_t driver_write(void *cookie, off_t position, const void *buffer,
+                             size_t *num_bytes) {
+  int length = *num_bytes;
+  if (length > FIFO_SIZE - charNum) {
+    length = FIFO_SIZE - charNum;
+  }
+  if (state == TESTDEVICE_STATE_DISABLE) {
+    printk("Device is disabled\n");
+    return num_bytes;
+  }
+  char *kbuf = malloc(length + 1);
+  if (kbuf == NULL) {
+    printk("Error in kmalloc\n");
+    return -ENOMEM;
+  }
+  int t = copy_from_user(kbuf, buffer, (unsigned long int)length);
+  if (t != 0) {
+    printk("Error in copy_from_user\n");
+    // return -EFAULT;
+  }
+  length = length - t;
+  kbuf[length] = '\0';
+  printk("Writing to device: %s\n", kbuf);
+  acquire_sem(rwMutex);
+  if (charNum + length + 1 > FIFO_SIZE) {
+    printk("FIFO is full\n");
+    return -ENOSPC;
+  }
+  for (int i = 0; i < length; i++) {
+    FIFO[charNum + i] = kbuf[i];
+  }
+  charNum = charNum + length;
+  FIFO[charNum] = '\0';
+  printk("FIFO: %s\n", FIFO);
+  release_sem(rwMutex);
 
-const char**
-publish_devices(void)
-{
-	static const char *names[] = {"test2/test/1", NULL};
-	dprintf("test: publish_devices\n");
-	return names;
+  *num_bytes = length;
+  return B_OK;
 }
 
-
-device_hooks*
-find_device(const char* name)
-{
-	static device_hooks hooks = {
-		driver_open,
-		driver_close,
-		driver_free,
-		driver_control,
-		driver_read,
-		driver_write,
-	};
-	dprintf("test: find_device\n");
-	return &hooks;
+static status_t driver_control(void *cookie, uint32 op, void *arg, size_t len) {
+  dprintf("test: control\n");
+  return B_ERROR;
 }
 
+const char **publish_devices(void) {
+  static const char *names[] = {"test2/test/1", NULL};
+  dprintf("test: publish_devices\n");
+  return names;
+}
+
+device_hooks *find_device(const char *name) {
+  static device_hooks hooks = {
+      driver_open,    driver_close, driver_free,
+      driver_control, driver_read,  driver_write,
+  };
+  dprintf("test: find_device\n");
+  return &hooks;
+}
